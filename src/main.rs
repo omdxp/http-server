6334ecf7a3fef3d92a172ba1baa399a86c9a660c
omdxp <omarbelghaouti@gmail.com>
2022-09-16 17:37:13 +0100

    define `Server` struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: &str) -> Self {
+        Self { addr: addr.into() }
+    }
+    fn run(&self) {
+        println!("server running at http://{}", self.addr)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1:8080");
+    server.run()
 }
