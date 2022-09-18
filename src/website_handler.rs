6f86d2de9cc8d6c15dd9eafc7026758cfda3e731
omdxp <omarbelghaouti@gmail.com>
2022-09-18 11:24:04 +0100

    route incoming requests

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,10 +1,17 @@
-use crate::http::{Request, Response, StatusCode};
+use crate::http::{Method, Request, Response, StatusCode};
 use crate::server::Handler;
 
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".into()))
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".into())),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".into())),
+                _ => Response::new(StatusCode::NotFound, None),
+            },
+            _ => Response::new(StatusCode::NotFound, None),
+        }
     }
 }
