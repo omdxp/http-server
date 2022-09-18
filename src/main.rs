0961faca0ae48fc4c526a410731a3ad6ae770f81
omdxp <omarbelghaouti@gmail.com>
2022-09-18 12:09:50 +0100

    serve html files

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ mod http;
 mod server;
 mod website_handler;
 use server::*;
+use std::env;
 use website_handler::WebsiteHandler;
 
 fn main() {
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
     let server = Server::new("127.0.0.1:8080");
-    server.run(WebsiteHandler)
+    server.run(WebsiteHandler::new(public_path))
 }
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,13 +1,26 @@
 use crate::http::{Method, Request, Response, StatusCode};
 use crate::server::Handler;
+use std::fs;
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+    public_path: String,
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        fs::read_to_string(path).ok()
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".into())),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".into())),
                 _ => Response::new(StatusCode::NotFound, None),
             },
