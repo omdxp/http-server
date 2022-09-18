012871c0c6c53f7f49ea2074d13b38e28fe83cf8
omdxp <omarbelghaouti@gmail.com>
2022-09-18 10:57:58 +0100

    write to tcp stream

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,3 +1,5 @@
+use std::{fmt::Display, io::Write, net::TcpStream};
+
 use super::StatusCode;
 
 #[derive(Debug)]
@@ -9,5 +11,18 @@ pub struct Response {
 impl Response {
     pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
         Self { status_code, body }
+    }
+    pub fn send(&self, stream: &mut TcpStream) -> std::io::Result<()> {
+        let body = match &self.body {
+            Some(b) => b,
+            None => "",
+        };
+        write!(
+            stream,
+            "HTTP/1.1 {} {}\r\n\r\n{}",
+            self.status_code,
+            self.status_code.reason_phrase(),
+            body
+        )
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,6 @@
-use crate::http::Request;
+use crate::http::{Request, Response, StatusCode};
 use std::convert::TryFrom;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 pub struct Server {
@@ -23,11 +23,21 @@ impl Server {
                             match tcp_stream.read(&mut buf) {
                                 Ok(_) => {
                                     println!("request received: {}", String::from_utf8_lossy(&buf));
-                                    match Request::try_from(&buf[..]) {
+                                    let response = match Request::try_from(&buf[..]) {
                                         Ok(request) => {
                                             dbg!(request);
+                                            Response::new(
+                                                StatusCode::Ok,
+                                                Some("<h1> It works</h1>".to_string()),
+                                            )
                                         }
-                                        Err(e) => println!("{:?}", e),
+                                        Err(e) => {
+                                            println!("{:?}", e);
+                                            Response::new(StatusCode::BadRequest, None)
+                                        }
+                                    };
+                                    if let Err(e) = response.send(&mut tcp_stream) {
+                                        println!("Failed to send response: {}", e)
                                     }
                                 }
                                 Err(e) => println!("{:?}", e),
